5d8e751a1c11ddbbcc6347f1711a8ed64372df97
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2026-02-06 23:54:19 +0900

    Feat: SEO를 위한 설명 문구 수정

--- apps/web/src/constants/metadata-info.ts
+++ apps/web/src/constants/metadata-info.ts
@@ -1,4 +1,6 @@
 export const METADATA_INFO = {
   SITE_NAME: '마음판',
-  SITE_DESCRIPTION: '흘러가는 말씀을 붙잡아, 인자와 진리를 마음에 새기다',
+  SITE_DESCRIPTION:
+    '듣고 흘러가는 설교, 이제 AI로 붙잡으세요. 마음판은 유튜브에 있는 목사님의 설교 영상을 핵심 내용으로 정리하고 성경 말씀 묵상과 암송을 도와주는 서비스입니다.',
+  OPEN_GRAPH_DESCRIPTION: '흘러가는 말씀을 붙잡아, 인자와 진리를 마음에 새기다',
 } as const;
