544a732c92f91cc0ba6bae748f98fe8e1b32bc7e
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2026-02-06 17:08:12 +0900

    Chore: 폰트를 컴포넌트로 다른 파일에 추출
    - 메타데이터 정보를 상수로 추출

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,36 +1,35 @@
 import './globals.css';
 
+import { maruburi } from '@/assets/fonts/fonts';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import { APP_BASE_URL } from '@/constants/app-path';
+import { METADATA_INFO } from '@/constants/metadata-info';
 import KakaoScript from '@/lib/kakao-script';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata, Viewport } from 'next';
-import localFont from 'next/font/local';
-
-const SITE_DESCRIPTION = '흘러가는 말씀을 붙잡아, 인자와 진리를 마음에 새기다';
 
 export const metadata: Metadata = {
   metadataBase: new URL(APP_BASE_URL),
-  title: '마음판',
-  description: SITE_DESCRIPTION,
+  title: METADATA_INFO.SITE_NAME,
+  description: METADATA_INFO.SITE_DESCRIPTION,
   openGraph: {
-    title: '마음판',
-    description: SITE_DESCRIPTION,
+    title: METADATA_INFO.SITE_NAME,
+    description: METADATA_INFO.SITE_DESCRIPTION,
     type: 'website',
     url: APP_BASE_URL,
-    siteName: '마음판',
+    siteName: METADATA_INFO.SITE_NAME,
   },
   twitter: {
     card: 'summary_large_image',
-    title: '마음판',
-    description: SITE_DESCRIPTION,
+    title: METADATA_INFO.SITE_NAME,
+    description: METADATA_INFO.SITE_DESCRIPTION,
   },
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
-    title: '마음판',
+    title: METADATA_INFO.SITE_NAME,
   },
 };
 
@@ -44,23 +43,6 @@ export const viewport: Viewport = {
   maximumScale: 1,
   userScalable: false,
 };
-
-const maruburi = localFont({
-  src: [
-    {
-      path: '../assets/fonts/maruburi/MaruBuri-Regular.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../assets/fonts/maruburi/MaruBuri-Bold.otf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-maruburi',
-  display: 'swap',
-});
 
 export default function RootLayout({
   children,
